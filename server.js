716c8e806464743967b5ebb9a4f23ec09d337694
dantedimon <dantedimon20@ukr.net>
2019-02-13 06:02:52 +0200

    deploy fix

--- server.js
+++ server.js
@@ -1,14 +1,10 @@
 let express = require('express');
 let serveStatic = require('serve-static');
 let path = require('path');
-let port = process.env.PORT || 80;
+let port = process.env.PORT || 5000;
 
 let app = express();
 
 app.use(serveStatic(__dirname + "/dist"));
-
-app.get('*', (request, response) => {
-    response.sendFile(path.resolve(__dirname + "/dist", 'index.html'));
-});
 
 app.listen(port);
