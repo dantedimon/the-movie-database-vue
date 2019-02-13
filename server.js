f005dd1739c545789ddbff70974af1713037f867
dantedimon <dantedimon20@ukr.net>
2019-02-13 06:06:14 +0200

    deploy fix

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 let express = require('express');
 let serveStatic = require('serve-static');
 let path = require('path');
-let port = process.env.PORT || 5000;
+let port = process.env.PORT || 80;
 
 let app = express();
 
